b3863f8298ce336fef81df734819d2ab97d870d8
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-02-20 02:52:12 +0530

    Api Model integration is done for the Status of the application.

--- app/src/main/java/com/easebuzz/testing/ixigoclone/Base/managers/ApiUriManager.java
+++ app/src/main/java/com/easebuzz/testing/ixigoclone/Base/managers/ApiUriManager.java
@@ -1,11 +1,17 @@
 package com.easebuzz.testing.ixigoclone.Base.managers;
 
+import javax.xml.transform.sax.SAXResult;
+
 public class ApiUriManager {
-    public static final String BASE_URL="https://pnr-status-for-railways-api.onrender.com";
-    public static final String BASE_TEST_URL="https://jsonplaceholder.typicode.com/";
+    public static final String BASE_URL="https://pnr-status-for-railways-api.onrender.com/";
+    public static final String BASE_TEST_URL="https://jsonplaceholder.typicode.com";
 
     //Pnr
     public static final String STATUS="/status";
     //Posts
     public static final String POSTS="/posts";
+
+    public  static final String COMMENTS="/posts/{id}/comments";
+    public  static final String COMMENTS_ID="/comments";
+
 }
--- app/src/main/java/com/easebuzz/testing/ixigoclone/Base/utils/ApiCallingMethods.java
+++ app/src/main/java/com/easebuzz/testing/ixigoclone/Base/utils/ApiCallingMethods.java
@@ -3,15 +3,29 @@ import com.easebuzz.testing.ixigoclone.Base.interfaces.ApiServices;
 import com.easebuzz.testing.ixigoclone.Base.interfaces.ResponseListener;
 import com.easebuzz.testing.ixigoclone.Base.interseptor.RetrofitClient;
 import com.easebuzz.testing.ixigoclone.Base.managers.ApiManager;
+import com.easebuzz.testing.ixigoclone.Base.models.Response.Comments;
 import com.easebuzz.testing.ixigoclone.Base.models.Response.Posts;
+import com.easebuzz.testing.ixigoclone.Base.models.Response.TrainInfo;
 
 import java.util.List;
 
 import retrofit2.Call;
 
-public class ApiCallingMethods {
+public class ApiCallingMethods extends ApiManager {
+    public static void getPnrStatus(ResponseListener listener, String pnr,Integer requestCode){
+        Call<TrainInfo> call=RetrofitClient.getRetrofit().create(ApiServices.class).getPnrStatus(pnr);
+        getRailwayData(listener,call,requestCode);
+    }
     public static void getAllPost(ResponseListener listener, Integer requestCode){
         Call<List<Posts>> call = RetrofitClient.getRetrofit().create(ApiServices.class).getPosts();
-        ApiManager.getData(listener,call,requestCode);
+        getData(listener,call,requestCode);
+    }
+    public static void getComments(ResponseListener listener,Integer requestCode,String id){
+        Call<List<Comments>> call= RetrofitClient.getRetrofit().create(ApiServices.class).getComments(id);
+        getData(listener,call,requestCode);
+    }
+    public static void getCommentsWithId(ResponseListener listener,Integer requestCode,String id){
+        Call<List<Comments>> call= RetrofitClient.getRetrofit().create(ApiServices.class).getCommentsWithId(id);
+        getData(listener,call,requestCode);
     }
 }
